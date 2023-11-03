81236438ab23147904978beb5ee4537265078f74
felix <felixokoth5@gmail.com>
2023-11-03 14:12:41 +0300

    inital set up and add entity

---
+++ src/test/java/com/ecommerce/start/ecommercebackend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.start.ecommercebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
